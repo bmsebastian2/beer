b81ef201d47a57d8f019bbebcc03631116de36d8
Sebastian <bmsebastian2@gmail.com>
2022-08-03 19:04:59 +0100

    Se agrego la animacion del vaso porque la habia borrado

--- src/elements/LetrasLiquida.jsx
+++ src/elements/LetrasLiquida.jsx
@@ -2,6 +2,29 @@ import "../style/LetrasLiquida.css";
 import Uy from "../elements/TituloBarUy";
 
 const LetrasLiquida = () => {
+
+
+  const json =
+  {
+  liqui: {
+    letterSpacing: "0.2vmin"
+  },
+  liqui_h1: {
+    fontFamily: "Edu VIC WA NT Beginner, cursive", 
+    fontSize: "18vmin", 
+    position: "absolute", 
+    top: "1rem", 
+    left: "45vw"
+  },
+  uno: {
+    fontSize: "17.9vmin !important"
+  },
+  wave: {
+    color: "#ff3d00", 
+    animation: "wave 3s ease-in-out infinite"
+  }
+}
+  
   return (
     <>
       <div className="liqui">
