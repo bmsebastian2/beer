f46bd88eced9c4cba38e67d3cbb61a60f6a822a4
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2022-08-17 17:02:49 +0100

    BARRA MENU VERSION 2

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -18,7 +18,7 @@ const Menu = () => {
   };
 
   return (
-    <div>
+    <div className="containerMenu">
       <menu className="menu">
         {vButtonActive.map((e, item) => (
           <ButtonMenu
@@ -32,34 +32,6 @@ const Menu = () => {
             clickItem={clickItem}
           />
         ))}
-
-        {/* <button
-          className="menu__item active"
-          style={{ "--bgColorItem": "#e0b115" }}
-        >
-          <svg className="icon" viewBox="0 0 24 24">
-            <path
-              d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
-        C3.9,4.4,4.4,3.9,5.1,3.9z"
-            />
-            <path d="M4.2,9.3h15.6" />
-            <path d="M9.1,9.5v10.3" />
-          </svg>
-        </button> */}
-
-        {/* <button
-          className="menu__item active"
-          style={{ "--bgColorItem": "#65ddb7" }}
-        >
-          <svg className="icon" viewBox="0 0 24 24">
-            <path
-              d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
-        C3.9,4.4,4.4,3.9,5.1,3.9z"
-            />
-            <path d="M5.5,20l9.9-9.9l4.7,4.7" />
-            <path d="M10.4,8.8c0,0.9-0.7,1.6-1.6,1.6c-0.9,0-1.6-0.7-1.6-1.6C7.3,8,8,7.3,8.9,7.3C9.7,7.3,10.4,8,10.4,8.8z" />
-          </svg>
-        </button> */}
 
         <div className="menu__border" />
       </menu>
