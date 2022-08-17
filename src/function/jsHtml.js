59af18bdb1ea63854ec04d0c751869a2f469bf70
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2022-08-17 12:47:38 +0100

    Ventana3

--- src/function/jsHtml.js
+++ src/function/jsHtml.js
@@ -1,8 +1,23 @@
 if (screen.width < 820) {
-  window.open(
-    "myPopupAd.html",
-    "My Popup Window",
-    "height=350,width=350,status=no,toolbar=no,menubar=no"
-  );
-  window.scrollTo(0, 1);
+  //   window.open(
+  //     "myPopupAd.html",
+  //     "My Popup Window",
+  //     "height=350,width=350,status=no,toolbar=no,menubar=no"
+  //   );
+  //   window.scrollTo(0, 1);
+  function abreSinNavegacion() {
+    open(
+      "index.html",
+      "principal",
+      "location=no,menubar=no,status=no,toolbar=no"
+    );
+    cerrar();
+  }
+
+  function cerrar() {
+    var ventana = window.self;
+    ventana.opener = window.self;
+    ventana.close();
+  }
+  abreSinNavegacion();
 }
