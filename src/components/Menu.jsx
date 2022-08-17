ff7d2491f11550541a7383dbb597953e37e4bba2
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2022-08-17 16:57:38 +0100

    BARRA MENU VERSION 1

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,63 +1,92 @@
-import '../style/menu.css'
-import listButton from '../function/listButtonMenu';
-import ButtonMenu from '../elements/ButtonMenu';
-import { useState } from 'react';
-
+import "../style/menu.css";
+import { listButton, InvertirButton } from "../function/listButtonMenu";
+import ButtonMenu from "../elements/ButtonMenu";
+import { useState } from "react";
 
 const Menu = () => {
+  const reset = listButton;
+  const [vButtonActive, setvButtonActive] = useState(reset);
 
-  const reset = listButton
-  const [vButtonActive, setvButtonActive] = useState(reset)
+  const clickItem = (id) => {
+    // if (e.target.classList[1] === "active") return;
 
+    let ver = InvertirButton(id, vButtonActive);
+    console.log(ver);
+    setvButtonActive(ver);
+    // let ar1 = vButtonActive.map((el) => el);
+    // console.log(ar1);
+  };
 
+  return (
+    <div>
+      <menu className="menu">
+        {vButtonActive.map((e, item) => (
+          <ButtonMenu
+            key={item}
+            id={item}
+            d1={e.d1}
+            d2={e.d2}
+            d3={e.d3}
+            color={e.color}
+            active={e.active}
+            clickItem={clickItem}
+          />
+        ))}
 
-  const clickItem = (e,id)=>{     
-    
- 
-    if (e.target.classList[1]==='active')  return
-   
-    // (vButtonActive[id].active ==='active')
-    let ar1 = (vButtonActive.map(el=>el))
-    console.log(ar1);
+        {/* <button
+          className="menu__item active"
+          style={{ "--bgColorItem": "#e0b115" }}
+        >
+          <svg className="icon" viewBox="0 0 24 24">
+            <path
+              d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
+        C3.9,4.4,4.4,3.9,5.1,3.9z"
+            />
+            <path d="M4.2,9.3h15.6" />
+            <path d="M9.1,9.5v10.3" />
+          </svg>
+        </button> */}
 
-    // setvButtonActive(...ar1)
-    // let ar2 = (vButtonActive.map((el,item)=>item !== id ? el:el.active='active'))
-    
+        {/* <button
+          className="menu__item active"
+          style={{ "--bgColorItem": "#65ddb7" }}
+        >
+          <svg className="icon" viewBox="0 0 24 24">
+            <path
+              d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
+        C3.9,4.4,4.4,3.9,5.1,3.9z"
+            />
+            <path d="M5.5,20l9.9-9.9l4.7,4.7" />
+            <path d="M10.4,8.8c0,0.9-0.7,1.6-1.6,1.6c-0.9,0-1.6-0.7-1.6-1.6C7.3,8,8,7.3,8.9,7.3C9.7,7.3,10.4,8,10.4,8.8z" />
+          </svg>
+        </button> */}
 
-    // const array = vButtonActive.filter(ele=>ele.active!=='active')
-    // console.log(array)
-   
-    // let array = []
-    // vButtonActive.map((element,items)=>{
-    //   if(items === id){
-    //     if(element.active==='active'){
+        <div className="menu__border" />
+      </menu>
 
-    //     }
-    //     else
-    //     {
-    //       element.active='active'
-    //       array.push(element)
-    //     }
+      <div className="svg-container">
+        <svg viewBox="0 0 202.9 45.5">
+          <clipPath
+            id="menu"
+            clipPathUnits="objectBoundingBox"
+            transform="scale(0.0049285362247413 0.021978021978022)"
+          >
+            <path
+              d="M6.7,45.5c5.7,0.1,14.1-0.4,23.3-4c5.7-2.3,9.9-5,18.1-10.5c10.7-7.1,11.8-9.2,20.6-14.3c5-2.9,9.2-5.2,15.2-7
+        c7.1-2.1,13.3-2.3,17.6-2.1c4.2-0.2,10.5,0.1,17.6,2.1c6.1,1.8,10.2,4.1,15.2,7c8.8,5,9.9,7.1,20.6,14.3c8.3,5.5,12.4,8.2,18.1,10.5
+        c9.2,3.6,17.6,4.2,23.3,4H6.7z"
+            />
+          </clipPath>
+        </svg>
+      </div>
+    </div>
+  );
+};
 
-    //   }else{
-    //     element.active=''
-    //     array.push(element)
-    //   }
-    // })
-    
-    // setvButtonActive(array)
-  
-  }
+export default Menu;
 
-
-
-
-
-return (
-
-<div>
-  <menu className="menu">
-    {/* <button className="menu__item active" style={{"--bgColorItem":"#ff8c00"}}>
+{
+  /* <button className="menu__item active" style={{"--bgColorItem":"#ff8c00"}}>
       <svg className="icon" viewBox="0 0 24 24">
         <path d="M3.8,6.6h16.4" />
         <path d="M20.2,12.1H3.8" />
@@ -77,48 +106,5 @@ return (
         <path d="M3.4,16.2l8.8,4.5l8.4-4.5" />
         <path d="M3.7,7.8l8.6-4.5l8,4.5l-8,4.3L3.7,7.8z" />
       </svg>
-      </button> */}
-     
-      {
-        vButtonActive.map((e,item)=> <ButtonMenu key={item} id = {item} d1={e.d1} d2={e.d2} d3={e.d3} color={e.color} active={e.active}  clickItem = {clickItem}/>)
-      }
-   
-    <button className="menu__item active" style={{"--bgColorItem":"#e0b115"}}> 
-      <svg className="icon" viewBox="0 0 24 24">
-        <path d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
-        C3.9,4.4,4.4,3.9,5.1,3.9z" />
-        <path d="M4.2,9.3h15.6" />
-        <path d="M9.1,9.5v10.3" />
-      </svg>
-    </button>
-    
-    <button className="menu__item active" style={{"--bgColorItem":"#65ddb7"}}>
-      <svg className="icon" viewBox="0 0 24 24">
-        <path d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
-        C3.9,4.4,4.4,3.9,5.1,3.9z" />
-        <path d="M5.5,20l9.9-9.9l4.7,4.7" />
-        <path d="M10.4,8.8c0,0.9-0.7,1.6-1.6,1.6c-0.9,0-1.6-0.7-1.6-1.6C7.3,8,8,7.3,8.9,7.3C9.7,7.3,10.4,8,10.4,8.8z" />
-      </svg>
-    </button>
-
-    <div className="menu__border" />
-  </menu>
-
-
-  <div className="svg-container">
-    <svg viewBox="0 0 202.9 45.5">
-      <clipPath id="menu" clipPathUnits="objectBoundingBox" transform="scale(0.0049285362247413 0.021978021978022)">
-        <path d="M6.7,45.5c5.7,0.1,14.1-0.4,23.3-4c5.7-2.3,9.9-5,18.1-10.5c10.7-7.1,11.8-9.2,20.6-14.3c5-2.9,9.2-5.2,15.2-7
-        c7.1-2.1,13.3-2.3,17.6-2.1c4.2-0.2,10.5,0.1,17.6,2.1c6.1,1.8,10.2,4.1,15.2,7c8.8,5,9.9,7.1,20.6,14.3c8.3,5.5,12.4,8.2,18.1,10.5
-        c9.2,3.6,17.6,4.2,23.3,4H6.7z" />
-      </clipPath>
-    </svg>
-  </div>
-</div>
- 
-)
-
+      </button> */
 }
-
-
-export default Menu
--- src/elements/ButtonMenu.jsx
+++ src/elements/ButtonMenu.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
-const ButtonMenu = ({id,d1,d2,d3,color,active, clickItem}) => {
-   
-    return (
-    
-    <button  onClick={(e)=>clickItem(e,id)} className={'menu__item ' + active}  style={{"--bgColorItem":`${color}`}}>
-      <svg className="icon" viewBox="0 0 24 24" >
+const ButtonMenu = ({ id, d1, d2, d3, color, active, clickItem }) => {
+  return (
+    <button
+      onClick={() => clickItem(id)}
+      className={"menu__item " + active}
+      style={{ "--bgColorItem": `${color}` }}
+    >
+      <svg className="icon" viewBox="0 0 24 24">
         <path d={d1} />
-        <path d={d2}  />
-        <path d={d3}  />
+        <path d={d2} />
+        <path d={d3} />
       </svg>
     </button>
+  );
+};
 
-    )
-}
-
-export default ButtonMenu
+export default ButtonMenu;
