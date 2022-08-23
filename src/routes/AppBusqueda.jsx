2440d676848bca1870e5c59025868cd57727680c
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2022-08-23 14:01:30 +0000

    OSE se agrego INPUT con los stylos,se necesitael CONTEXT de la ubicación

--- src/routes/AppBusqueda.jsx
+++ src/routes/AppBusqueda.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Menu from "../components/Menu";
+import Modal from "../components/Modal";
 
 const AppBusqueda = () => {
   const Conta = styled.div`
@@ -10,6 +11,7 @@ const AppBusqueda = () => {
     opacity: 100%;
     display: flex;
     flex-direction: column;
+    color:#fff;
 
     @media (min-width: 320px) {
     }
@@ -21,16 +23,54 @@ const AppBusqueda = () => {
     }
   `;
   const Header = styled.div`
-    flex: 2;
+    flex: 1;
     background: linear-gradient(220.55deg, #565656 0%, #181818 100%);
+    display:grid;    
+    grid-template-columns:'1fr 1fr';
+    padding:2rem;
+    // border:1px solid blue;
+    
 
     @media (min-width: 1200px) {
       border-top-left-radius: 3rem;
       border-top-right-radius: 3rem;
     }
   `;
+  const HeaderIconos = styled.div`
+   display:grid;
+   grid-template-columns:2fr 1fr;
+  //  border:1px solid yellow;
+   
+
+  `
+  const HeaderBuscar = styled.div`
+   display:grid;  
+   align-self: end;
+  //  border:1px solid red;
+  
+
+   `
+   const H3 =  styled.h3`
+   font-family: Edu VIC WA NT Beginner, cursive;
+     font-size: 6vmin;
+   `
+
+  const Ancla = styled.a`
+      padding: 7px;
+     font-size: 19px;
+     border-width: 0px;
+     border-color: #ffffff;
+     background-color: #fd683d;
+     max-height:3rem;
+     color: rgb(29, 38, 48);;
+     border-style: solid;
+     border-radius: 13px;
+     box-shadow: -1px -2px 5px rgba(66,66,66,.36);      
+  
+  `
+  
   const Body = styled.div`
-    flex: 6;
+    flex: 7;
     background: white;
   `;
 
@@ -45,7 +85,23 @@ const AppBusqueda = () => {
   return (
     <>
       <Conta>
-        <Header />
+        <Header>
+
+          <HeaderIconos>
+            <div className="" style={{display:'flex', alignItems:'center'}}>
+              <H3>Bares de Montevideo.</H3>
+            </div>
+            <div className="" style={{display:'flex', alignItems:'center', justifyContent:'end'}}>
+              2
+            </div>
+
+          </HeaderIconos>
+
+          <HeaderBuscar>
+            <Ancla href="#modal-tree" >Ingrese</Ancla>
+          </HeaderBuscar>
+            <Modal id="modal-tree" title="Seleccione ubicación." window={false} />
+        </Header>
         <Body />
         <Footer>
           <Menu />
