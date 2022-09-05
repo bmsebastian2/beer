3ff5b66ea61bb4108e4002be4949dc4c4e23a019
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2022-09-05 20:31:32 +0000

    OSE Ventana IngresarebARes

--- src/routes/IngresarBares.jsx
+++ src/routes/IngresarBares.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+
+const ContainerMain = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+
+`
+const ContainerMenu = styled.div`
+  height: 50vh;
+  width: 20%;
+background: linear-gradient(220.55deg, #FFF6EB 0%, #DFD1C5 100%);
+  border-radius: 1.5rem;  
+`
+
 
 const IngresarBares = () => {
-  return <div>IngresarBares</div>;
+  return (
+  <ContainerMain>
+
+    <ContainerMenu>
+
+    </ContainerMenu>
+
+  </ContainerMain>
+  )
 };
 
 export default IngresarBares;
