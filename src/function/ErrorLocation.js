3f04c17a6b7c013cb31527fb35881019d919e7d1
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2022-09-07 10:42:59 +0100

    RES- Se elimino ApisTATE YA QUE NO SE ESTAB UTILIZANDO SOLO PARA EJEMPLO, Se anexo correcion locationPosition En el fetch de la ubicacion

--- src/function/ErrorLocation.js
+++ src/function/ErrorLocation.js
@@ -1,12 +1,10 @@
-
 const error = {
-    1:'Habilite GPS en el navegador.'
-}
+  1: "Habilite GPS en el navegador.",
+  2: "Error en GeoCode. Seleccione manualmente su ubicación.",
+};
 
 const searcherror = (key) => {
- return error[key]?error[key]:'Other error'
-}
+  return error[key] ? error[key] : "Other error";
+};
 
-
-
-export default searcherror
+export default searcherror;
--- src/function/locationPosition.jsx
+++ src/function/locationPosition.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import searcherror from '../function/ErrorLocation'
+import searcherror from "../function/ErrorLocation";
 export const apiStates = {
   LOADING: "LOADING",
   SUCCESS: "SUCCESS",
@@ -10,34 +10,32 @@ export const locationPosition = () => {
   const [data, setData] = React.useState({
     state: apiStates.LOADING,
     error: "",
-    district:'',
-    countryName:'',
-    city:'',
+    district: "",
+    countryName: "",
+    city: "",
   });
   const setPartData = (partialData) => setData({ ...data, ...partialData });
-  
+
   React.useEffect(() => {
     setPartData({
       state: apiStates.LOADING,
     });
     const showPosition = (position) => {
-
-      if(position){        
-        getUbicacion(position, setPartData)     
-      }    
-      
-    }
+      if (position) {
+        getUbicacion(position, setPartData);
+      }
+    };
     function gpsError(err) {
       setPartData({
         state: apiStates.ERROR,
-        error: searcherror(err.code),//codigo de error en este caso validamos el 1 que es inicio de gps en el navegador
+        error: searcherror(err.code), //codigo de error en este caso validamos el 1 que es inicio de gps en el navegador
       });
       showPosition();
     }
     const opciones = {
       enableHighAccuracy: true,
-      maximumAge: 0,//dispositivo no puede usar una posición almacenada en caché
-      timeout: 60000,// tardar para devolver una posición
+      maximumAge: 0, //dispositivo no puede usar una posición almacenada en caché
+      timeout: 60000, // tardar para devolver una posición
     };
 
     const getLocation = () => {
@@ -53,31 +51,34 @@ export const locationPosition = () => {
   return data;
 };
 
-function getUbicacion(position, setPartData)  {  
-  
-      let la = position.coords.latitude
-      let lo = position.coords.longitude
+function getUbicacion(position, setPartData) {
+  let la = position.coords.latitude;
+  let lo = position.coords.longitude;
 
-    fetch(
-      `https://revgeocode.search.hereapi.com/v1/revgeocode?at=${la},${lo}&lang=en-US&apikey=1W9Uzy8_P4PvjP1ZfO6NkAbCRyuuL0D3uXqMDvzLer8`
-    )
-      .then((resp) => resp.json())
-      .then((data) => {
-        const {
-          // label,
-          // houseNumber,
-          // postalCode,
-          // street,
-          countryName,
-          city,
-          district,
-        } = data.items[0].address;      
-       setPartData({
+  fetch(
+    `https://revgeocode.search.hereapi.com/v1/revgeocode?at=${la},${lo}&lang=en-US&apikey=1W9Uzy8_P4PvjP1ZfO6NkAbCRyuuL0D3uXqMDvzLer8`
+  )
+    .then((resp) => resp.json())
+    .then((data) => {
+      const {
+        // label,
+        // houseNumber,
+        // postalCode,
+        // street,
+        countryName,
+        city,
+        district,
+      } = data.items[0].address;
+      setPartData({
         state: apiStates.SUCCESS,
         district,
         countryName,
         city,
+      }).catch((error) => {
+        setPartData({
+          state: apiStates.ERROR,
+          error: searcherror(2), //codigo de error en este caso validamos el 1 que es inicio de gps en el navegador
+        });
       });
-      })      
-
-};
+    });
+}
