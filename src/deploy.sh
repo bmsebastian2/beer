964461c9f2f2b1b730180a35e3e50bb8be3afea4
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2022-09-07 15:56:24 +0100

    GH PAGES VERSION 4

--- src/deploy.sh
+++ src/deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:bmsebastian2/<REPO>beer.git main:gh-pages
+# git push -f git@github.com:bmsebastian2/beer.git main:gh-pages
 
 cd -
